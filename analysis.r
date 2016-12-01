184a75272f5d84d3fe47dfa4152f6735bea9720e
armstrongmsg <armstrongmsg@gmail.com>
2016-12-01 18:19:01 -0300

    Added the experiment and data analysis scripts.

---
+++ analysis.r
@@ -0,0 +1,32 @@
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+
+data <- read.csv("results.csv")
+
+data$cap_a <- as.factor(data$cap_a)
+data$cap_b <- as.factor(data$cap_b)
+
+conf.intervals <- data %>% group_by(cap_a, cap_b) %>% summarise(mean_time_a = mean(total_time_a),
+                                              mean_time_b = mean(total_time_b),
+                                              lower_time_a = wilcox.test(total_time_a, conf.int = TRUE)$conf.int[1],
+                                              upper_time_a = wilcox.test(total_time_a, conf.int = TRUE)$conf.int[2],
+                                              lower_time_b = wilcox.test(total_time_b, conf.int = TRUE)$conf.int[1],
+                                             upper_time_b = wilcox.test(total_time_b, conf.int = TRUE)$conf.int[2]) 
+
+set1 <- subset(conf.intervals, select = c(cap_a, cap_b, mean_time_b, lower_time_b, upper_time_b))
+set1$instance = rep("B", 6)
+colnames(set1) <- c("cap_a", "cap_b", "mean_time", "lower_time", "upper_time", "instance")
+
+set2 <- subset(conf.intervals, select = c(cap_a, cap_b, mean_time_a, lower_time_a, upper_time_a))
+set2$instance = rep("A", 6)
+colnames(set2) <- c("cap_a", "cap_b", "mean_time", "lower_time", "upper_time", "instance")
+
+final_intervals <- rbind(set1, set2)
+final_intervals$case <- c(paste(as.character(final_intervals$cap_a),as.character(final_intervals$cap_b), sep = "-"))
+
+limits <- aes(ymax = final_intervals$upper_time, ymin = final_intervals$lower_time)
+
+ggplot(final_intervals, aes(x=case, y=mean_time, color = instance)) + 
+  geom_point() + 
+  geom_errorbar(limits)
---
+++ experiment.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+function log {
+	echo $1 >> $LOG_FILE
+}
+
+function factorial() 
+  if (( $1 < 2 ))
+  then
+    echo 1
+  else
+    echo "$1 * $(factorial $(( $1 - 1 )))" | bc
+  fi
+
+function limit_affinity {
+  INSTANCE_A=$1
+  INSTANCE_B=$2
+
+  virsh vcpupin $INSTANCE_A 0 0
+  virsh vcpupin $INSTANCE_B 0 0
+}
+
+function reset_affinity {
+  INSTANCE_A=$1
+  INSTANCE_B=$2
+  
+  N_CPUS="`cat /proc/cpuinfo | grep processor | wc -l`"
+  virsh vcpupin $INSTANCE_A 0 0-$(( $N_CPUS-1 ))
+  virsh vcpupin $INSTANCE_B 0 0-$(( $N_CPUS-1 ))
+}
+
+function run {
+	TIMES=$3
+
+	virsh schedinfo $INSTANCE_A --set vcpu_quota=$(( $1*1000 )) > /dev/null
+	virsh schedinfo $INSTANCE_B --set vcpu_quota=$(( $2*1000 )) > /dev/null
+
+	for i in `seq 1 $TIMES`
+	do
+		echo "Starting applications"
+		ssh $VM_A_USER@$INSTANCE_A_IP "$(typeset -f); { time -p factorial $N > /dev/null; } 2>&1 | grep "real" > log.txt" &
+		ssh $VM_B_USER@$INSTANCE_B_IP "$(typeset -f); { time -p factorial $N > /dev/null; } 2>&1 | grep "real" > log.txt" &
+
+		CPU_USAGE=""
+
+		echo "Waiting for applications to finish"
+		while [ "`ssh $VM_A_USER@$INSTANCE_A_IP cat log.txt`" = "" ]
+		do
+			sleep 1
+			CPU_IDLE=`sar -P 0 1 1 | awk 'NR==4 { print $8 } ' | awk -F "," '{ print $1 }'`
+			CPU_USAGE="$CPU_USAGE $(( 100 - $CPU_IDLE ))"
+		done
+	
+		TOTAL_TIME_A="`ssh $VM_A_USER@$INSTANCE_A_IP cat log.txt | awk '{ print $2}' | awk -F "," '{print $1}'`"
+		ssh $VM_A_USER@$INSTANCE_A_IP rm log.txt
+	
+		while [ "`ssh $VM_B_USER@$INSTANCE_B_IP cat log.txt`" = "" ]
+		do
+			sleep 1
+		done
+	
+		TOTAL_TIME_B="`ssh $VM_B_USER@$INSTANCE_B_IP cat log.txt | awk '{ print $2 }' | awk -F "," '{print $1}'`"
+	
+		ssh $VM_B_USER@$INSTANCE_B_IP rm log.txt
+
+		CPU_USAGE="`echo $CPU_USAGE | xargs`"
+		log "$1,$2,$TOTAL_TIME_A,$TOTAL_TIME_B,$CPU_USAGE"
+	done
+}
+
+N=$1
+TIMES=$2
+INSTANCE_A_IP="192.168.122.94"
+INSTANCE_B_IP="192.168.122.202"
+INSTANCE_A="ubuntu"
+INSTANCE_B="ubuntu1"
+VM_A_USER="ubuntu"
+VM_B_USER="ubuntu"
+LOG_FILE="results.csv"
+
+echo "Setting affinities"
+limit_affinity $INSTANCE_A $INSTANCE_B
+
+log "cap_a,cap_b,total_time_a,total_time_b,cpu_usage"
+
+echo "Running with conf:CAP A=100%, CAP B=100%"
+run 100 100 $TIMES
+echo "Running with conf:CAP A=100%, CAP B=50%"
+run 100 50 $TIMES
+echo "Running with conf:CAP A=50%, CAP B=50%"
+run 50 50 $TIMES
+echo "Running with conf:CAP A=70%, CAP B=30%"
+run 70 30 $TIMES
+echo "Running with conf:CAP A=50%, CAP B=30%"
+run 50 30 $TIMES
+echo "Running with conf:CAP A=80%, CAP B=60%"
+run 80 60 $TIMES
+
+echo "Resetting affinities"
+reset_affinity $INSTANCE_A $INSTANCE_B
