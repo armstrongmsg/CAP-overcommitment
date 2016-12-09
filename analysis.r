93b20b61974c0414a45e98ff074cd122efa85322
armstrongmsg <armstrongmsg@gmail.com>
2016-12-09 16:31:35 -0300

    Added more plots to analysis

--- analysis.r
+++ analysis.r
@@ -25,8 +25,87 @@ colnames(set2) <- c("cap_a", "cap_b", "mean_time", "lower_time", "upper_time", "
 final_intervals <- rbind(set1, set2)
 final_intervals$case <- c(paste(as.character(final_intervals$cap_a),as.character(final_intervals$cap_b), sep = "-"))
 
-limits <- aes(ymax = final_intervals$upper_time, ymin = final_intervals$lower_time)
+# -----------------------------------------------------------------------------------
+# Question 2
+# -----------------------------------------------------------------------------------
 
-ggplot(final_intervals, aes(x=case, y=mean_time, color = instance)) + 
+
+# -----------------------------------------------------------------------------------
+# Cases 1,2 and 3
+# -----------------------------------------------------------------------------------
+
+final_intervals123 <- final_intervals %>% filter(case %in% c("100-50", "100-100", "100-30", "50-50", "80-20"))
+
+limits <- aes(ymax = final_intervals123$upper_time, ymin = final_intervals123$lower_time)
+
+ggplot(final_intervals123, aes(x=case, y=mean_time, color = instance)) + 
   geom_point() + 
   geom_errorbar(limits)
+
+ggsave("results123.png")
+
+# -----------------------------------------------------------------------------------
+# Cases 4,5 and 6
+# -----------------------------------------------------------------------------------
+
+final_intervals456 <- final_intervals %>% filter(case %in% c("30-30", "50-30", "80-60"))
+
+limits <- aes(ymax = final_intervals456$upper_time, ymin = final_intervals456$lower_time)
+
+ggplot(final_intervals456, aes(x=case, y=mean_time, color = instance)) + 
+  geom_point() + 
+  geom_errorbar(limits)
+
+ggsave("results456.png")
+
+# -----------------------------------------------------------------------------------
+# Different CAPs over 100%
+# ----------------------------------------------------------------------------------
+
+final_intervals_over100_1 <- final_intervals %>% filter(case %in% c("100-100", "100-50", "100-30"))
+
+limits <- aes(ymax = final_intervals_over100_1$upper_time, ymin = final_intervals_over100_1$lower_time)
+
+ggplot(final_intervals_over100_1, aes(x=case, y=mean_time, color = instance)) + 
+  geom_point() + 
+  geom_errorbar(limits)
+
+ggsave("results_over100_1.png")
+
+final_intervals_over100_2 <- final_intervals %>% filter(case %in% c("80-60", "80-20"))
+
+limits <- aes(ymax = final_intervals_over100_2$upper_time, ymin = final_intervals_over100_2$lower_time)
+
+ggplot(final_intervals_over100_2, aes(x=case, y=mean_time, color = instance)) + 
+  geom_point() + 
+  geom_errorbar(limits)
+
+ggsave("results_over100_2.png")
+
+final_intervals_over100_3 <- final_intervals %>% filter(case %in% c("100-100", "100-50", "50-50", "80-60"))
+
+limits <- aes(ymax = final_intervals_over100_3$upper_time, ymin = final_intervals_over100_3$lower_time)
+
+ggplot(final_intervals_over100_3, aes(x=case, y=mean_time, color = instance)) + 
+  geom_point() + 
+  scale_y_continuous(limits = c(26,31)) +
+  geom_errorbar(limits)
+
+ggsave("results_over100_3.png")
+
+# -----------------------------------------------------------------------------------
+# CAP effect comparison
+# ----------------------------------------------------------------------------------
+
+final_intervals_capcomparison <- final_intervals %>% filter(case %in% c("30-30", "50-30", "100-100", "100-50", "50-50", "80-60", "80-20"))
+
+final_intervals_capcomparison$case2 <- factor(final_intervals_capcomparison$case, levels = c("30-30", "50-30", "50-50", "80-20", "80-60", "100-50", "100-100"))
+limits <- aes(ymax = final_intervals_capcomparison$upper_time, ymin = final_intervals_capcomparison$lower_time)
+
+ggplot(final_intervals_capcomparison, aes(x=case2, y=mean_time, color = instance)) + 
+  geom_point() + 
+  geom_errorbar(limits)
+
+ggsave("results_capeffect_comparison.png")
+
+ggsave("results.png")
--- experiment.sh
+++ experiment.sh
@@ -71,12 +71,12 @@ function run {
 
 N=$1
 TIMES=$2
-INSTANCE_A_IP="192.168.122.94"
-INSTANCE_B_IP="192.168.122.202"
-INSTANCE_A="ubuntu"
-INSTANCE_B="ubuntu1"
-VM_A_USER="ubuntu"
-VM_B_USER="ubuntu"
+INSTANCE_A_IP="192.168.122.23"
+INSTANCE_B_IP="192.168.122.210"
+INSTANCE_A="overcommitment1"
+INSTANCE_B="overcommitment2"
+VM_A_USER="armstrong"
+VM_B_USER="armstrong"
 LOG_FILE="results.csv"
 
 echo "Setting affinities"
@@ -85,21 +85,21 @@ limit_affinity $INSTANCE_A $INSTANCE_B
 log "cap_a,cap_b,total_time_a,total_time_b,cpu_usage"
 
 echo "Running with conf:CAP A=100%, CAP B=100%"
-#run 100 100 $TIMES
+run 100 100 $TIMES
 echo "Running with conf:CAP A=100%, CAP B=50%"
-#run 100 50 $TIMES
+run 100 50 $TIMES
 echo "Running with conf:CAP A=50%, CAP B=50%"
-#run 50 50 $TIMES
+run 50 50 $TIMES
 echo "Running with conf:CAP A=70%, CAP B=30%"
-#run 70 30 $TIMES
+run 80 20 $TIMES
 echo "Running with conf:CAP A=50%, CAP B=30%"
-#run 50 30 $TIMES
+run 50 30 $TIMES
 echo "Running with conf:CAP A=80%, CAP B=60%"
-#run 80 60 $TIMES
+run 80 60 $TIMES
 echo "Running with conf:CAP A=30%, CAP B=30%"
-#run 30 30 $TIMES
+run 30 30 $TIMES
 echo "Running with conf:CAP A=70%, CAP B=40%"
-#run 70 40 $TIMES
+run 70 40 $TIMES
 echo "Running with conf:CAP A=100%, CAP B=30%"
 run 100 30 $TIMES
 
